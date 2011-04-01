96691e25bd1f8c83c023b50ac57cb35b7111255b
rdlester <sursiks.010@gmail.com>
2011-04-01 13:21:58 -0400

    PostOffice stubs

--- hermes/PostOffice.java
+++ hermes/PostOffice.java
@@ -1,5 +1,36 @@
 package hermes;
 
-public class PostOffice {
+import oscP5.*;
+import netP5.*;
 
+/**
+ * Listens for and sends OSC, mouse, and keyboard messages
+ * Tells subscribers of a specific type of message when one is recieved
+ * and passes on information stored in message to subscriber
+ * @author Ryan
+ *
+ */
+public class PostOffice {
+	
+	/**
+	 * 
+	 */
+	public PostOffice() {
+		
+	}
+	
+	/**
+	 * 
+	 * @param b
+	 */
+	public void registerSubscription(Being b) {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void run() {
+		
+	}
 }
