2346f121cc82bda0428dba528da122e76ab9328b
Sam Eilertsen <samuel_eilertsen@brown.edu>
2011-04-01 13:16:28 -0400

    added basic run functionality

--- hermes/Being.java
+++ hermes/Being.java
@@ -8,11 +8,11 @@ import java.util.Collection;
  * Basic game object class
  * Anything that is getting drawn or interacting with other game objects is a Being
  */
-abstract class Being {
+public abstract class Being {
 
-	abstract double getX();
-	abstract double getY();
-	abstract double getZ();
+	public abstract double getX();
+	public abstract double getY();
+	public abstract double getZ();
 
 	/**
 	 * draws the object to the screen
@@ -20,12 +20,12 @@ abstract class Being {
 	 * the reason for this is that the being's internal x and y will be screen coordinates, not world coordinates
 	 * there may be a better way to do this, with framebuffers, need to look in to this
 	 */
-	abstract void draw(int x, int y);
+	public abstract void draw(int x, int y);
 
 	List<Collection> groups;
 	Shape shape; 
 	
-	void update() {}
+	public void update() {}
 	
 	//TODO: need to add methods for recieving messages?
 	//Has to be defined in Being
--- hermes/World.java
+++ hermes/World.java
@@ -1,5 +1,7 @@
 package hermes;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Collection;
 
@@ -16,27 +18,68 @@ public abstract class World {
 	
 	Camera camera;
 	
-	//Setup loop
-	//Written by user
-	abstract void setup();
-
-	void registerEnvironment(Environment environment) {}
-
-	void registerInteraction(Collection A, Collection B, Interactor inter) {}
-	void registerInteraction(Collection A, Collection B, Interactor inter, Optimizer optimizer) {}
+	private List<Interaction> interactions;
+	
+	public World() {
+		interactions = new LinkedList<Interaction>();
+	}
+	
+	/**
+	 * register an interaction to be handled on the update loop
+	 * @param A					the first interacting group
+	 * @param B					the second interacting group
+	 * @param inter				the interaction handler
+	 * @param applyImmediate	whether to apply the interaction immediately
+	 * 								upon detection or later
+	 */
+	public void registerInteraction(Collection A, Collection B, Interactor inter, 
+			boolean applyImmediate) {
+		interactions.add(new Interaction(A, B, inter, applyImmediate, null));
+	}
+	
+	/**
+	 * register an interaction to be handled on the update loop
+	 * @param A			the first interacting group
+	 * @param B			the second interacting group
+	 * @param inter		the interaction handler
+	 */
+	public void registerInteraction(Collection A, Collection B, Interactor inter, 
+			boolean applyImmediate, Optimizer optimizer) {
+		interactions.add(new Interaction(A, B, inter, applyImmediate, optimizer));
+	}
 
 	//Creates a new thread and runs the update loop in it
-	void run() {}
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void run() {
+		for(Iterator<Interaction> iter = interactions.iterator(); iter.hasNext(); ) {
+			System.out.println("running...");
+			Interaction interaction = iter.next();
+			Collection A = interaction.getA();
+			Collection B = interaction.getB();
+			for(Iterator iterA = A.iterator(); iterA.hasNext(); ) {
+				for(Iterator iterB = B.iterator(); iterB.hasNext(); ) {
+					Being being1 = (Being)iterA.next();
+					Being being2 = (Being)iterB.next();
+					
+					if(interaction.getInteractor().detect(being1, being2)) {
+						interaction.getInteractor().handle(being1, being2);
+					}
+				}
+			}
+		}
+	}
 
 	//Basic method to call to know if world is running or not
-	boolean getState() {
+	public boolean getState() {
 		return false;
 	}
 
 	//Called by God's draw method to
-	void draw() {}
+	public void draw() {}
 	
 	// locks the update rate to happen no more than once per interval (in seconds)
-	void lockUpdateRate(double interval) {}
+	public void lockUpdateRate(double interval) {}
 
+	
+	
 }
