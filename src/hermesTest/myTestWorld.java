9abcf0f22faa17dd0cf31a6ad626385630ab9153
Sam Eilertsen <samuel_eilertsen@brown.edu>
2011-04-01 14:11:52 -0400

    moved test files

---
+++ src/hermesTest/myTestWorld.java
@@ -0,0 +1,22 @@
+package src.hermesTest;
+
+import java.util.ArrayList;
+
+import src.hermes.*;
+
+public class myTestWorld extends World {
+
+	myTestWorld() {
+		super();
+		ArrayList<myTestBeing> group = new ArrayList<myTestBeing>();
+		group.add(new myTestBeing());
+		registerInteraction(group, group, 
+				new myTestInteractor(), true);
+	}
+
+	public static void main(String args[]) {
+		myTestWorld world = new myTestWorld();
+		world.run();
+	}
+	
+}
