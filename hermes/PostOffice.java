040b8beba23c4f9cd6762ecc5ffbed46e72cdcfb
rdlester <sursiks.010@gmail.com>
2011-04-01 13:26:48 -0400

    Better PO stubs

--- hermes/PostOffice.java
+++ hermes/PostOffice.java
@@ -1,5 +1,8 @@
 package hermes;
 
+import java.util.Collection;
+import java.util.ArrayList;
+
 import oscP5.*;
 import netP5.*;
 
@@ -12,25 +15,51 @@ import netP5.*;
  */
 public class PostOffice {
 	
-	/**
-	 * 
-	 */
+	//Fields containing subscribing Beings
+	ArrayList<Being> keySubscribed;
+	ArrayList<Being> mouseSubscribed;
+	ArrayList<Being> oscSubscribed;
+	
+	//Constructor
 	public PostOffice() {
 		
 	}
 	
-	/**
-	 * 
-	 * @param b
-	 */
-	public void registerSubscription(Being b) {
+	//Sends key presses to subscribing Beings
+	void handleKeyPress(char keyPressed) {
+		
+	}
+
+	//Sends mouse clicks to subscribing Beings
+	void handleMouseClick(Point clickLocation) {
+		
+	}
+
+	//Sends osc messages to subscribing Beings
+	//Q: how will we internally represent OSC messages?
+	void handleOSC(Message recievedMessage) {
+		
+	}
+
+	//Registers Group of Being for keyboard subscription
+	void registerKeySubscribe(Collection g) {
+		
+	}
+
+	//Registers Group of Being for mouse subscription
+	void registerMouseSubscribe(Collection g) {
+		
+	}
+
+	//Registers Group of Being for osc subscription
+	void registerOSCSubscribe(Collection g) {
 		
 	}
 	
-	/**
-	 * 
-	 */
-	public void run() {
+	//Runs Post Office
+	//Waits for events, and adds them to queue for handling
+	//Does not handle events while a world update loop is running
+	void run() {
 		
 	}
 }
