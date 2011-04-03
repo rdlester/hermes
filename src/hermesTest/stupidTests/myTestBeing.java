dfeb56dadafc21a8ba5f65db05cebb1ef4f79e75
Sam Eilertsen <samuel_eilertsen@brown.edu>
2011-04-03 15:25:08 -0400

    moved test files to sub-package

---
+++ src/hermesTest/stupidTests/myTestBeing.java
@@ -0,0 +1,36 @@
+package src.hermesTest.stupidTests;
+
+import src.hermes.*;
+
+public class myTestBeing extends Being {
+
+	@Override
+	public double getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getZ() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean isTestBeing() {
+		return true;
+	}
+	
+	@Override
+	public void draw(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
---
+++ src/hermesTest/stupidTests/myTestWorld.java
@@ -0,0 +1,24 @@
+package src.hermesTest.stupidTests;
+/*package src.hermesTest;
+
+import java.util.ArrayList;
+
+import src.hermes.*;
+
+public class myTestWorld extends World {
+
+	myTestWorld() {
+		super();
+		ArrayList<myTestBeing> group = new ArrayList<myTestBeing>();
+		group.add(new myTestBeing());
+		registerInteraction(group, group, 
+				new myTestInteractor(), true);
+	}
+
+	public static void main(String args[]) {
+		myTestWorld world = new myTestWorld();
+		world.run();
+	}
+	
+}
+*/
