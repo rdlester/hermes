ba0514be52e496a038fc1bfb223c135bff7c0190
Sam Eilertsen <samuel_eilertsen@brown.edu>
2011-04-13 15:16:54 -0400

    added Shape class

--- src/hermes/Shape.java
+++ src/hermes/Shape.java
@@ -1,5 +1,26 @@
 package src.hermes;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Shape {
+
+	private LinkedList<Shape> _contained;
+	
+	public boolean collide(Shape other) {
+		boolean result = false;
+		for(Iterator<Shape> iter = _contained.iterator(); iter.hasNext(); ) {
+			result = result && iter.next().collide(other);
+		}
+		return result;
+	}
+	
+}
+
+public class Circle {
+	
+	public boolean collide(Circle other) {
+		
+	}
 	
 }
