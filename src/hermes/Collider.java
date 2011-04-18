468350d942d9b116d700328bb6d2847357692e91
Sam Eilertsen <samuel_eilertsen@brown.edu>
2011-04-18 02:47:10 -0400

    added MassedBeing class

--- src/hermes/Collider.java
+++ src/hermes/Collider.java
@@ -3,17 +3,13 @@ package src.hermes;
 import src.hermes.shape.Shape;
 
 /**
- * Following  to interact two beings
- * detect method determines if two bodies should interact
- * handle "interacts" the two beings by updating them
+ * interactor that detects collisions between Beings based on their shape
  */
 public abstract class Collider<A extends Being, B extends Being> implements Interactor<A, B>{
 
-
 	// returns true if A and B collide
 	public boolean detect(A being1, B being2) {
-		//Shape.collide(being1.getShape(), being2.getShape());
-		return true;
+		return being1.getShape().collide(being2.getShape());
 	}
 
 	// handles an interaction between A and B
