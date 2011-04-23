1f16038cc0cd87d6ed0bffa8bd003e1b99b20cb2
chris <chris@Chris-Novellos-MacBook-Pro.local>
2011-04-23 17:35:47 -0400

    PostOffice changed from OscMessage to OSCMessage.. using in-place references to com.illposed.osc

--- src/hermes/postoffice/OscMessage.java
+++ src/hermes/postoffice/OscMessage.java
@@ -1,14 +1,13 @@
 package src.hermes.postoffice;
 
-import com.illposed.osc.*;
 
 /**
  * Message representing an OSC message
  * Very similar to illposed's OSCMessage, but implementing Message so it can be used in PostOffice
- * The type of an OscMessage is determined by the address it is sent on
+ * The type of an OSCMessage is determined by the address it is sent on
  */
-//TODO: change the name to something that conflicts less with illposed
-public class OscMessage implements Message {
+
+public class OSCMessage implements Message {
 	
 	//Address of OSCMessage
 	private String _address;
@@ -17,10 +16,10 @@ public class OscMessage implements Message {
 	
 	/**
 	 * Basic constructor for unpacked OSCMessage
-	 * @param address - Address of OscMessage
-	 * @param contents - Contents of OscMessage
+	 * @param address - Address of OSCMessage
+	 * @param contents - Contents of OSCMessage
 	 */
-	public OscMessage(String address, Object[] contents) {
+	public OSCMessage(String address, Object[] contents) {
 		_address = address;
 		_contents = contents;
 	}
@@ -29,7 +28,7 @@ public class OscMessage implements Message {
 	 * Constructor taking an illposed OSCMessage
 	 * @param message - a packed OSCMessage in illposed's format
 	 */
-	public OscMessage(OSCMessage message) {
+	public OSCMessage(com.illposed.osc.OSCMessage message) {
 		_address = message.getAddress();
 		_contents = message.getArguments();
 	}
@@ -53,14 +52,14 @@ public class OscMessage implements Message {
 	/**
 	 * Get an illposed version of this message
 	 */
-	public OSCMessage toIllposed() {
-		OSCMessage m = new OSCMessage(_address,_contents);
+	public com.illposed.osc.OSCMessage toIllposed() {
+		com.illposed.osc.OSCMessage m = new com.illposed.osc.OSCMessage(_address,_contents);
 		return m;
 	}
 	
 	public boolean equals(Object o) {
-		if(o instanceof OscMessage) {
-			OscMessage m = (OscMessage) o;
+		if(o instanceof OSCMessage) {
+			OSCMessage m = (OSCMessage) o;
 			if(this.getAddress() == m.getAddress()) return true;
 		}
 		return false;
--- src/hermes/postoffice/PostOffice.java
+++ src/hermes/postoffice/PostOffice.java
@@ -17,7 +17,6 @@ import java.util.HashMap;
 import java.util.PriorityQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-import com.illposed.osc.*;
 
 import processing.core.*;
 import src.hermes.GenericGroup;
@@ -33,9 +32,9 @@ public class PostOffice implements KeyListener, MouseListener, MouseMotionListen
 	private static final boolean DEBUG = true;
 	
 	//OSCPorts for listening and receiving
-	private OSCPortIn _receive;
-	private OSCPortOut _send;
-	private OSCListener _listener;
+	private com.illposed.osc.OSCPortIn _receive;
+	private com.illposed.osc.OSCPortOut _send;
+	private com.illposed.osc.OSCListener _listener;
 	
 	//Map that associates the subscriptions with the Message they want to receive
 	private HashMap<Message, ArrayList<Subscription>> _subscriptions;
@@ -46,7 +45,7 @@ public class PostOffice implements KeyListener, MouseListener, MouseMotionListen
 	/**
 	 * Implementation of OCSListener that turns illposed messages into our messages
 	 */
-	class PostOfficeOSCListener implements OSCListener {
+	class PostOfficeOSCListener implements com.illposed.osc.OSCListener {
 		
 		//Reference to containing post office
 		PostOffice _p;
@@ -63,8 +62,8 @@ public class PostOffice implements KeyListener, MouseListener, MouseMotionListen
 		 * Converts it from illposed to our message
 		 * Adds it to queue
 		 */
-		public void acceptMessage(Date time, OSCMessage message) {
-			OscMessage m = new OscMessage(message);
+		public void acceptMessage(Date time, com.illposed.osc.OSCMessage message) {
+			OSCMessage m = new OSCMessage(message);
 			_p._messageQueue.add(m);
 		}
 	}
@@ -96,14 +95,14 @@ public class PostOffice implements KeyListener, MouseListener, MouseMotionListen
 		Hermes.getPApplet().addMouseWheelListener(this);
 		//Start OSC and set listener
 		try {
-			_receive = new OSCPortIn(portIn);
+			_receive = new com.illposed.osc.OSCPortIn(portIn);
 			_receive.startListening();
 		} catch (SocketException e) {
 			System.err.println("OSC Port In on " + portIn + " could not start");
 			e.printStackTrace();
 		}
 		try {
-			_send = new OSCPortOut();
+			_send = new com.illposed.osc.OSCPortOut();
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -131,14 +130,14 @@ public class PostOffice implements KeyListener, MouseListener, MouseMotionListen
 		Hermes.getPApplet().addMouseWheelListener(this);
 		//Start OSC and set listener
 		try {
-			_receive = new OSCPortIn(portIn);
+			_receive = new com.illposed.osc.OSCPortIn(portIn);
 			_receive.startListening();
 		} catch (SocketException e) {
 			System.err.println("OSC Port In on " + portIn + " could not start");
 			e.printStackTrace();
 		}
 		try {
-			_send = new OSCPortOut(InetAddress.getLocalHost(),portOut);
+			_send = new com.illposed.osc.OSCPortOut(InetAddress.getLocalHost(),portOut);
 		} catch (UnknownHostException e) {
 			System.err.println("OSC Port Out on " + portOut + " could not start");
 			e.printStackTrace();
@@ -167,14 +166,14 @@ public class PostOffice implements KeyListener, MouseListener, MouseMotionListen
 		Hermes.getPApplet().addMouseWheelListener(this);
 		//Start OSC and set listener
 		try {
-			_receive = new OSCPortIn(portIn);
+			_receive = new com.illposed.osc.OSCPortIn(portIn);
 			_receive.startListening();
 		} catch (SocketException e) {
 			System.err.println("OSC Port In on " + portIn + " could not start");
 			e.printStackTrace();
 		}
 		try {
-			_send = new OSCPortOut(InetAddress.getByName(netAddress), portOut);
+			_send = new com.illposed.osc.OSCPortOut(InetAddress.getByName(netAddress), portOut);
 		} catch (UnknownHostException e) {
 			System.err.println("OSC Port Out on " + portOut + " could not start");
 			e.printStackTrace();
@@ -196,14 +195,14 @@ public class PostOffice implements KeyListener, MouseListener, MouseMotionListen
 	public PostOffice() {
 		//Start OSC to listen on 8000 and output on 8080, same as monome
 		try {
-			_receive = new OSCPortIn(8000);
+			_receive = new com.illposed.osc.OSCPortIn(8000);
 			_receive.startListening();
 		} catch (SocketException e) {
 			System.err.println("OSC Port In on 8000 could not start");
 			e.printStackTrace();
 		}
 		try {
-			_send = new OSCPortOut(InetAddress.getLocalHost(), 8080);
+			_send = new com.illposed.osc.OSCPortOut(InetAddress.getLocalHost(), 8080);
 		} catch (UnknownHostException e) {
 			System.err.println("OSC Port Out on 8080 could not start");
 			e.printStackTrace();
@@ -238,8 +237,8 @@ public class PostOffice implements KeyListener, MouseListener, MouseMotionListen
 			subscriptionList.add(newSubscription);
 			_subscriptions.put(check, subscriptionList);
 		}
-		if(check instanceof OscMessage) {
-			OscMessage osc = (OscMessage) check;
+		if(check instanceof OSCMessage) {
+			OSCMessage osc = (OSCMessage) check;
 			_receive.addListener(osc.getAddress(), _listener);
 		}
 	}
@@ -267,8 +266,8 @@ public class PostOffice implements KeyListener, MouseListener, MouseMotionListen
 	/**
 	 * Sends a provided message out into the world
 	 */
-	public void sendMail(OscMessage m) {
-		OSCMessage mail = m.toIllposed();
+	public void sendMail(OSCMessage m) {
+		com.illposed.osc.OSCMessage mail = m.toIllposed();
 		try {
 			_send.send(mail);
 		}
