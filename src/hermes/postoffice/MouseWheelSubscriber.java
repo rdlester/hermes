197e0a7754580605ff0c93160ee1bfe83f4accca
rdlester <sursiks.010@gmail.com>
2011-04-29 15:56:12 -0400

    fixed

--- src/hermes/postoffice/MouseWheelSubscriber.java
+++ src/hermes/postoffice/MouseWheelSubscriber.java
@@ -1,5 +1,5 @@
 package src.hermes.postoffice;
 
 public interface MouseWheelSubscriber {
-	public void handleMouseWheelSubscriber(MouseWheelMessage m);
+	public void handleMouseWheelMessage(MouseWheelMessage m);
 }
