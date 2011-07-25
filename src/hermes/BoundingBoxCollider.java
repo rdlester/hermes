9c15981e813f95166b02c0fa0563989323b00ca1
Sam Eilertsen <samuel_eilertsen@brown.edu>
2011-07-25 13:27:16 -0700

    cleaning up comments, updating core classes for new Interactor

--- src/hermes/BoundingBoxCollider.java
+++ src/hermes/BoundingBoxCollider.java
@@ -1,20 +1,14 @@
 package src.hermes;
 
 /**
- * basic collision detector, detect will return true if the bounding boxes of the beings overlap
- * @author Sam
- *
- * @param <A>	the first Being type
- * @param <B>	the second Being type
+ * A basic collision detector. <code>detect</code> will return true if the bounding boxes of the beings overlap.
+ * The implementation of <code>handle</code> is left to the child classes.
  */
-public abstract class BoundingBoxCollider<A extends Being, B extends Being> implements Interactor<A, B> {
+public abstract class BoundingBoxCollider<A extends Being, B extends Being> extends Interactor<A, B> {
 
-	// returns true if A and B collide
+	// returns true if the bounding boxes of A and B collide
 	public boolean detect(A being1, B being2) {
 		return being1.getBoundingBox().collide(being2.getBoundingBox());
 	}
 
-	// handles an interaction between A and B
-	public abstract boolean handle(A being1, B being2);
-	
 }
--- src/hermes/CameraBeingInteractor.java
+++ src/hermes/CameraBeingInteractor.java
@@ -1,19 +1,14 @@
 package src.hermes;
 
 /**
- * primarily for internal purposes -- you do not need to understand this class
- * 
- * adds beings to the camera's draw list if they intersect with the camera's bounding box
- * 
- * @author Sam
  *
+ * Used by World. Adds Beings to the Camera's draw list if they are on screen.
+ * 
  */
-public class CameraBeingInteractor extends BoundingBoxCollider<Camera, Being> {
-
-	//detect has been implemented in BoundingBoxCollider
-	public boolean handle(Camera being1, Being being2) {
+class CameraBeingInteractor extends BoundingBoxCollider<Camera, Being> {
+	
+	public void handle(Camera being1, Being being2) {
 		being1.addBeing(being2);
-		return true;
 	}
 
 
