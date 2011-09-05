33a856ae7cb8d5e356f79aff15c49e9f152ca34a
rdlester <sursiks.010@gmail.com>
2011-09-05 15:30:12 -0400

    Comments for Interactors and Collider updated

--- src/hermes/Optimizer.java
+++ src/hermes/Optimizer.java
@@ -1,11 +1,11 @@
 package src.hermes;
 
 /**
- * Determines which beings are compared and potentially interacted by Interactor
- * Group B is given as argument to constructor and organized according to optimizer type
- * Unoptimized interactions check all members of A against all the members of B (order n^2)
- * Sample advanced optimizers include quadtree
- * detect checks body A against the structure formed from Group B
+ * Determines which beings are compared and potentially interacted by <code>Interactor</code>.
+ * Group B is given as argument to constructor and organized according to optimizer type.
+ * Unoptimized interactions check all members of A against all the members of B (order n^2).
+ * Sample advanced optimizers include quadtree.
+ * Detect checks body A against the structure formed from Group B.
  */
 public interface Optimizer<A extends HObject, B extends HObject, 
 				GroupA extends GenericGroup<A,?>, GroupB extends GenericGroup<B,?>> {
