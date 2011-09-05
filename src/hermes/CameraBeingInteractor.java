1532403f44843213bb13fa56b9ece22c47ce6541
rdlester <sursiks.010@gmail.com>
2011-09-05 17:28:33 -0400

    More comments

--- src/hermes/CameraBeingInteractor.java
+++ src/hermes/CameraBeingInteractor.java
@@ -1,9 +1,9 @@
 package src.hermes;
 
 /**
- *
  * Used by World. Adds Beings to the Camera's draw list if they are on screen.
- * 
+ * <p>s
+ * Internal -- you do not need to understand this class!
  */
 class CameraBeingInteractor extends BoundingBoxCollider<HCamera, Being> {
 	
